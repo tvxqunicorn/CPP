560287cf3cdb3526e5fa3661371416054f93368f
Pepe <xinyun.pepe@gmail.com>
2021-09-17 15:11:15 +0200

    upgraded main.cpp

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: xli <xli@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 13:08:32 by xli               #+#    #+#             */
-/*   Updated: 2021/09/17 09:24:42 by xli              ###   ########lyon.fr   */
+/*   Updated: 2021/09/17 15:10:26 by xli              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,42 @@
 
 int main()
 {
+
+/*
+** -------------------- CONSTRUCTOR EXCEPTIONS --------------------
+*/
+
 	try
 	{
-		Bureaucrat Bob("Bob", 150);
-		std::cout << Bob << std::endl;
-		Bureaucrat Mike("Mike", 151);
-		std::cout << Mike << std::endl;
+		Bureaucrat Jane("Jane", 0);
+
+		std::cout << Jane << std::endl;
 	}
 	catch (std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
+
 	try
 	{
-		Bureaucrat Timmy("Timmy", 1);
-		std::cout << Timmy << std::endl;
-		Bureaucrat Michelle("Michelle", 0);
-		std::cout << Michelle << std::endl;
+		Bureaucrat Joe("Joe", 151);
+
+		std::cout << Joe << std::endl;
 	}
 	catch (std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
-	Bureaucrat Poppy("Poppy", 150);
-	std::cout << Poppy << std::endl;
+
+/*
+** -------------------- GRADE CHANGING EXCEPTIONS --------------------
+*/
+
 	try
 	{
+		Bureaucrat	Poppy("Poppy", 150);
+
+		std::cout << Poppy << std::endl;
 		std::cout << "Decrementing... " << std::endl;
 		Poppy.downGrade();
 	}
@@ -47,14 +57,34 @@ int main()
 	{
 		std::cerr << e.what() << std::endl;
 	}
-	Bureaucrat Zoe("Zoe", 1);
-	std::cout << Zoe << std::endl;
+
 	try
 	{
+		Bureaucrat	Zoe("Zoe", 1);
+
+		std::cout << Zoe << std::endl;
 		std::cout << "Incrementing... " << std::endl;
 		Zoe.upGrade();
 	}
 	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+/*
+** -------------------- COPY --------------------
+*/
+
+	try
+	{
+		Bureaucrat	Timmy("Timmy", 10);
+		Bureaucrat	Michelle("Michelle", 20);
+
+		std::cout << Timmy << "; " << Michelle << std::endl;
+		Michelle = Timmy;
+		std::cout << Timmy << "; " <<Michelle << std::endl;
+	}
+	catch (std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
