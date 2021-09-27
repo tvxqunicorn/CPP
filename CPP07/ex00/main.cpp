a76a166503701115a2cdee9e390b5a76bfe478f6
Pepe <xinyun.pepe@gmail.com>
2021-09-27 12:08:40 +0200

    cpp07ex00

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,43 +6,29 @@
 /*   By: xli <xli@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 11:17:49 by xli               #+#    #+#             */
-/*   Updated: 2021/09/27 12:06:03 by xli              ###   ########lyon.fr   */
+/*   Updated: 2021/09/27 12:08:05 by xli              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-int main() //why adding :: before functions?
+int main( void ) //why adding :: before functions?
 {
-	int	a = 10;
-	int	b = 20;
+	int a = 2;
+	int b = 3;
 
-	std::cout << "a: " << a << "; " << "b :" << b << std::endl;
-	std::cout << "----------SWAP----------" << std::endl;
-	::swap(a, b);
-	std::cout << "a: " << a << "; " << "b :" << b << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	std::cout << "----------MIN----------" << std::endl;
-	std::cout << "min: " << ::min(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 
-	std::cout << "----------MAX----------" << std::endl;
-	std::cout << "max: " << ::max(a, b) << std::endl;
-
-	std::cout << std::endl;
-
-	std::string	s1 = "Hello";
-	std::string	s2 = "Hi";
-
-	std::cout << "s1: " << s1 << "; " << "s2: " << s2 << std::endl;
-	std::cout << "----------SWAP----------" << std::endl;
-	::swap(s1, s2);
-	std::cout << "s1: " << s1 << "; " << "s2: " << s2 << std::endl;
-
-	std::cout << "----------MIN----------" << std::endl;
-	std::cout << "min: " << ::min(s1, s2) << std::endl;
-
-	std::cout << "----------MAX----------" << std::endl;
-	std::cout << "max: " << ::max(s1, s2) << std::endl;
-
-	return (0);
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	return 0;
 }
